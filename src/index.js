989df25954be12f89a36d118f72f1b94c973fb56
Unknown <sevenflash@yandex.ru>
2018-11-02 18:55:55 +0300

    fix sorting

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const state = new EthState({ redisClient, swapsName: config.ethSwaps, reputation
 const stateRouter = express.Router()
 
 stateRouter.get('/ethbtc', async (req, res) => {
-  const from = req.query.from || 0
+  const from = req.query.from || -1
   const limit = req.query.limit || 10
 
   const swaps = await state.fetchSwaps({ from, limit })
