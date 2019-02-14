5e72a34b5bb135f0f9405f832aa4dc7fb9e9ae51
Unknown <sevenflash@yandex.ru>
2019-02-14 12:35:33 +0200

    tokens state

--- src/state.js
+++ src/state.js
@@ -1,10 +1,11 @@
 const { promisify } = require("util")
 
 class State {
-  constructor({ redisClient, swapsName, swapsBitcoinName, reputationName }) {
+  constructor({ redisClient, swapsName, swapsBitcoinName, swapsTokensName, reputationName }) {
     this.redisClient = redisClient
     this.swapsName = swapsName
     this.swapsBitcoinName = swapsBitcoinName
+    this.swapsTokensName = swapsTokensName
     this.reputationName = reputationName
 
     this.get = promisify(this.redisClient.get).bind(this.redisClient)
@@ -21,28 +22,26 @@ class State {
 
   async fetchSwapRaw(swapSecretHash) {
     const bitcoin = await this.hgetall(`${this.swapsBitcoinName}:${swapSecretHash.replace(/^0x/, '')}`)
+
     const deposit = await this.hgetall(`${this.swapsName}:${swapSecretHash}:deposit`)
     const withdraw = await this.hgetall(`${this.swapsName}:${swapSecretHash}:withdraw`)
     const refund = await this.hgetall(`${this.swapsName}:${swapSecretHash}:refund`)
 
+    const tokensDeposit = await this.hgetall(`${this.swapsTokensName}:${swapsSecretHash}:deposit`)
+    const tokensWithdraw = await this.hgetall(`${this.swapsTokensName}:${swapsSecretHash}:withdraw`)
+    const tokensRefund = await this.hgetall(`${this.swapsTokensName}:${swapsSecretHash}:refund`)
+
     return {
-      bitcoin, deposit, withdraw, refund
+      bitcoin, deposit, withdraw, refund, tokensDeposit, tokensWithdraw, tokensRefund
     }
   }
 
-  async fetchSwap(swapSecretHash) {
+  async aggregateSwap({ swapBitcoin, swapDepositEvent, swapWithdrawEvent, swapRefundEvent }) {
     const swapAggregated = {
       status: 'waiting',
       alice: {},
       bob: {}
     }
-
-    const {
-      bitcoin: swapBitcoin,
-      deposit: swapDepositEvent,
-      withdraw: swapWithdrawEvent,
-      refund: swapRefundEvent
-    } = await this.fetchSwapRaw(swapSecretHash)
 
     if (swapDepositEvent) {
       const buyerAddress = swapDepositEvent.buyer
@@ -92,34 +91,81 @@ class State {
       }}
 
       swapAggregated.bob = { ...swapAggregated.bob, ...{
-          value: value,
-          fee: withdrawFee,
-          from: {
-            address: sellerAddress,
-            reputation: sellerReputation
-          },
-          transactions: { withdrawTx, fundingAddress }
+        value: value,
+        fee: withdrawFee,
+        from: {
+          address: sellerAddress,
+          reputation: sellerReputation
+        },
+        transactions: { withdrawTx, fundingAddress }
       }}
     }
 
     return swapAggregated
   }
 
-  async fetchSwaps({ from = -1, limit = 0 } = {}) {
+  async fetchTokensSwap(swapSecretHash) {
+    const {
+      bitcoin: swapBitcoin,
+      tokensDeposit: swapDepositEvent,
+      tokensWithdraw: swapWithdrawEvent,
+      tokensRefund: swapRefundEvent
+    }
+
+    const aggregatedSwap = await aggregateSwap({ swapBitcoin, swapDepositEvent, swapWithdrawEvent, swapRefundEvent })
+
+    return {
+      ...aggregatedSwap,
+      alice: {
+        ...aggregatedSwap.alice,
+        asset: 'Token'
+      }
+    }
+  }
+
+  async fetchSwap(swapSecretHash) {
+    const {
+      bitcoin: swapBitcoin,
+      deposit: swapDepositEvent,
+      withdraw: swapWithdrawEvent,
+      refund: swapRefundEvent
+    } = await this.fetchSwapRaw(swapSecretHash)
+
+    return aggregateSwap({ swapBitcoin, swapDepositEvent, swapWithdrawEvent, swapRefundEvent })
+  }
+
+  async fetchTokensSwaps({ from = -1, limit = 0 } = {}) {
+    return fetchSwaps({
+      fetchSwap: this.fetchTokensSwap,
+      swapsName: this.swapsTokensName,
+      from, limit
+    })
+  }
+
+  async fetchEthereumSwaps({ from = -1, limit = 0 } = {}) {
+    return fetchSwaps({
+      fetchSwap: this.fetchSwap,
+      swapsName: this.swapsName,
+      from, limit
+    })
+  }
+
+  async fetchSwaps({ from = -1, limit = 0, swapsName, fetchSwap } = {}) {
     let swaps = []
 
     let index = from
     if (from === -1) {
-      index = await this.llen(this.swapsName) - 1
+      index = await this.llen(swapsName) - 1
     }
 
     let hasEnded = false
 
     while(!hasEnded) {
-      const swapSecretHash = await this.lrange(this.swapsName, index, index)
+      const swapSecretHash = await this.lrange(swapsName, index, index)
 
       if (swapSecretHash[0]) {
-        const swap = await this.fetchSwap(swapSecretHash[0])
+        const swap = await fetchSwap(swapSecretHash[0])
+
         swaps.push(swap)
 
         if ((limit === 0 || swaps.length < limit) && (from !== -1 || index > 0)) {
